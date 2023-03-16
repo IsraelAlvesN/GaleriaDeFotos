f0006c859a7fd31335a9f490793e9172df9cd010
Israel Alves <devisrael98@gmail.com>
2023-03-16 15:33:27 -0300

    Função de loading na tela e puxando informações do firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import {useState, useEffect} from 'react'
 import * as C from './App.styles'
+import * as Photos from './services/photos'
+import { Photo } from './types/Photo'
 
 const App = () => {
+  const[loading, setLoading] = useState(false)
+  const[photos, setPhotos] = useState<Photo[]>([])
+
+  useEffect(() => {
+    const getPhotos = async() => {
+      setLoading(true)
+      setPhotos(await Photos.getAll())
+      setLoading(false)
+    }
+    getPhotos()
+  })
+
   return(
     <C.Container>
       <C.Area>
@@ -10,6 +25,12 @@ const App = () => {
         
 
         {/* Area de fotos */}
+        {loading &&
+          <C.ScreenWarning>
+            <div className='emoji'>✋</div>
+            <div>Carregando</div>
+          </C.ScreenWarning>
+        }
       </C.Area>
     </C.Container>
   )
